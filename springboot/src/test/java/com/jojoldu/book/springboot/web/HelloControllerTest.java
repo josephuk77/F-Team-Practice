f11c2450f623f8f3c9a3c101e65ddb1588cdfe35
aristo <dkfud2121@naver.com>
2023-03-22 17:46:16 +0900

    fix : error for testing code

--- springboot/src/test/java/com/jojoldu/book/springboot/web/HelloControllerTest.java
+++ springboot/src/test/java/com/jojoldu/book/springboot/web/HelloControllerTest.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -17,7 +16,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class HelloControllerTest {
     @Autowired
     private MockMvc mvc;
-
     @Test
     public void hello가_리턴된다() throws Exception{
         String hello = "hello";
@@ -26,4 +24,6 @@ public class HelloControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(content().string(hello));
     }
+
+
 }
