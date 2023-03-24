0c817517258a69ce578f89d5cdf1742e7e2aac7c
aristo <dkfud2121@naver.com>
2023-03-24 15:52:24 +0900

    feat : complete chapter 02

--- springboot/src/main/java/com/jojoldu/book/springboot/web/dto/HelloResponseDto.java
+++ springboot/src/main/java/com/jojoldu/book/springboot/web/dto/HelloResponseDto.java
@@ -1,2 +1,11 @@
-package com.jojoldu.book.springboot.web.dto;public class HelloResponseDto {
+package com.jojoldu.book.springboot.web.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class HelloResponseDto {
+    private final String name;
+    private final int amount;
 }
