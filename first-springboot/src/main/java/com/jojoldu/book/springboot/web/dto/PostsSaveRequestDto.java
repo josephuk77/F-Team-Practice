c0147b5bd2529f775e64c8772969c57248a105a0
josephuk77 <josephuk77@gmail.com>
2023-03-30 23:49:55 +0900

    h2 connection

---
+++ first-springboot/src/main/java/com/jojoldu/book/springboot/web/dto/PostsSaveRequestDto.java
@@ -0,0 +1,26 @@
+package com.jojoldu.book.springboot.web.dto;
+
+import com.jojoldu.book.springboot.domain.posts.Posts;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class PostsSaveRequestDto {
+    private String title;
+    private String content;
+    private String author;
+
+    @Builder
+    public PostsSaveRequestDto(String title, String content, String author){
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+
+
+    public Posts toEntity(){
+        return Posts.builder().author(author).content(content).title(title).build();
+    }
+}
