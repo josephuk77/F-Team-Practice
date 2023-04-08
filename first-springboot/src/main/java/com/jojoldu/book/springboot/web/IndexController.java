de0229f24313036f31ca87788c118eef5e251ed3
josephuk77 <josephuk77@gmail.com>
2023-04-08 23:00:13 +0900

    google oauth error

--- first-springboot/src/main/java/com/jojoldu/book/springboot/web/IndexController.java
+++ first-springboot/src/main/java/com/jojoldu/book/springboot/web/IndexController.java
@@ -1,24 +1,46 @@
 package com.jojoldu.book.springboot.web;
 
+import com.jojoldu.book.springboot.config.auth.dto.SessionUser;
+import com.jojoldu.book.springboot.web.dto.PostsResponseDto;
 import com.jojoldu.book.springboot.service.posts.PostsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+
+import javax.servlet.http.HttpSession;
 
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
+    private final HttpSession httpSession;
     private final PostsService postsService;
+
+
     @GetMapping("/")
     public String index(Model model){
         model.addAttribute("posts",postsService.findAllDesc());
+        SessionUser user =(SessionUser) httpSession.getAttribute("user");
+        if(user != null){
+            model.addAttribute("userName",user.getName());
+        }
         return "index";
     }
-
     @GetMapping("/posts/save")
     public String postsSave(){
         return "posts-save";
     }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model){
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+
+        return  "posts-update";
+    }
+
+
 }
