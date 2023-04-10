9edf5d92b9f961ca01f3bbeafc72c4c56254fe56
aristo <dkfud2121@naver.com>
2023-04-10 17:53:10 +0900

    feat : complete 5.4 어노테이션 기반으로 개선하기

---
+++ springboot/src/main/java/com/jojoldu/book/springboot/config/auth/LoginUserArgumentResolver.java
@@ -0,0 +1,36 @@
+package com.jojoldu.book.springboot.config.auth;
+
+import com.jojoldu.book.springboot.config.auth.dto.SessionUser;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+import javax.servlet.http.HttpSession;
+
+@RequiredArgsConstructor
+@Component
+public class LoginUserArgumentResolver implements HandlerMethodArgumentResolver {
+    private final HttpSession httpSession;
+    // 컨트롤러 메서드의 특정 파라미터를 지원하는 지 판단
+    // LoginUser 어노테이션이 있고, 파라미터 클래스 타입이 SessionUser.class 인 경우 참값 반환
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter){
+        boolean isLoginUserAnnotation = parameter.getParameterAnnotation(LoginUser.class)!=null;
+        boolean isUserClass = SessionUser.class.equals((parameter.getParameterType()));
+        return isLoginUserAnnotation && isUserClass;
+    }
+
+    // 파라미터에 전달할 객체 생성. 세션에서 객체를 가져온다.
+    @Override
+    public Object resolveArgument(MethodParameter parameter,
+                                  ModelAndViewContainer mavContainer,
+                                  NativeWebRequest webRequest,
+                                  WebDataBinderFactory binderFactory) throws Exception{
+        return httpSession.getAttribute("user");
+    }
+}
--- springboot/src/main/java/com/jojoldu/book/springboot/web/IndexController.java
+++ springboot/src/main/java/com/jojoldu/book/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.jojoldu.book.springboot.web;
 
+import com.jojoldu.book.springboot.config.auth.LoginUser;
 import com.jojoldu.book.springboot.config.auth.dto.SessionUser;
 import com.jojoldu.book.springboot.service.posts.PostsService;
 import com.jojoldu.book.springboot.web.dto.PostsResponseDto;
@@ -16,12 +17,11 @@ import javax.servlet.http.HttpSession;
 @Controller
 public class IndexController {
     private final PostsService postsService;
-    private final HttpSession httpSession;
 
+    // 어느 컨트롤러든지 LoginUser만 사용하면 세션 정보를 가져올 수 있음
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model, @LoginUser SessionUser user){
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user= (SessionUser) httpSession.getAttribute("user");
         if(user!= null){
             model.addAttribute("userName", user.getName());
         }
