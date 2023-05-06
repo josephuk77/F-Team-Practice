db10e9c9a9d5c5fbe518dc305b7a39281d745c41
josephuk77 <josephuk77@gmail.com>
2023-05-06 22:46:39 +0900

    board
    게시판

--- first-springboot/src/main/java/com/jojoldu/book/springboot/domain/user/User.java
+++ first-springboot/src/main/java/com/jojoldu/book/springboot/domain/user/User.java
@@ -1,5 +1,6 @@
 package com.jojoldu.book.springboot.domain.user;
 
+import com.jojoldu.book.springboot.domain.posts.BaseTimeEntity;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,7 +10,7 @@ import javax.persistence.*;
 @Getter
 @NoArgsConstructor
 @Entity
-public class User {
+public class User extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
